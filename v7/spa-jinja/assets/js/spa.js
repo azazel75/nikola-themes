17c7e7cd81241aa1f7e39ad53eb706fc8f34a134
Alberto Berti <alberto@metapensiero.it>
2014-11-08 02:40:40 +0100

    Add extrajs region to the rendered templates
    Also, updtate the context calculation to the latest exposed by the spa plugin

--- v7/spa-jinja/assets/js/spa.js
+++ v7/spa-jinja/assets/js/spa.js
@@ -32,7 +32,7 @@
         };
         Marionette.Renderer.render = function(template, data){
             template = Marionette.TemplateCache.get(template);
-            var blog_data = globals.to_template_context({post: data});
+            var blog_data = globals.to_template_context(data);
             return template.render(blog_data);
         };
         var Spa = Marionette.Application.extend();
@@ -71,19 +71,44 @@
             goToRoot: function() {
                 this.showModel('index.html.json');
             },
+
+            showModel: function(model_id) {
+                var self = this,
+                    posts = this.getOption('posts'),
+                    post = posts.get(model_id);
+
                 if (post)
-                    renderPost(post);
+                    renderPost(post.toJSON());
                 else {
-                    post = new models.Post({id: real_id});
+                    post = new models.Post({id: model_id});
                     posts.add(post);
                     post.fetch().then(renderPost);
+                }
+
+                function renderPost(post_or_d) {
+                    var view, app, extrajs_view,
+                        template_data = globals.get('client_templates')[post_or_d['template_name']],
+                        content_tmpl = template_data['view-content'],
+                        extrajs_tmpl = template_data['view-extrajs'];
+                    view = new ContentView({model: post, template: content_tmpl,
+                                            router: self});
+                    app = self.getOption('app');
+                    if (extrajs_tmpl) {
+                        extrajs_view = new Marionette.ItemView({model: post, template: extrajs_tmpl,
+                                                                router: self});
+                        app.extrajs_region.show(extrajs_view);
+                    } else
+                        app.extrajs_region.reset();
+                    app.content_region.show(view);
                 }
             }
         });
         var spa = new Spa();
         spa.addInitializer(function(options) {
-            this.addRegions({content_region: '#view-content'});
-            var router = new ContentRouter({posts: options.posts, app: this});
+            this.addRegions({content_region: '#view-content',
+                             extrajs_region: '#view-extrajs'});
+            var router = new ContentRouter({posts: options.posts, app:
+                                            this});
             enableScripts();
             Backbone.history.start({pushState: true});
         });
